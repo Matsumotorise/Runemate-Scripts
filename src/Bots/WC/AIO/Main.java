9f08c180a0809cdf443ecefbe60ee49d6bccc8cf
Andrew <andrewclchan211@gmail.com>
2018-05-29 10:14:54 -0400

    Initial prep

--- src/Bots/WC/AIO/Main.java
+++ src/Bots/WC/AIO/Main.java
@@ -4,25 +4,6 @@ import com.runemate.game.api.script.framework.LoopingBot;
 
 public class Main extends LoopingBot {
 
-	private enum State{
-
-	}
-
-	private State getCurrentState(){
-		return null;
-	}
-
-	@Override
-	public void onLoop() {
-		setLoopDelay(250, 401);
-
-
-		switch (getCurrentState()){
-
-		}
-
-	}
-
 	@Override
 	public void onStart(String... args){
 		super.onStart();
@@ -33,4 +14,56 @@ public class Main extends LoopingBot {
 	public void onStop(){
 		super.onStop();
 	}
+
+	private enum State{
+		CHOP, WALK_TO_CHOP, BANK, WALK_TO_BANK, DROP
+	}
+
+	private State getCurrentState(){
+		return null;
+	}
+
+
+	private void chop(){
+
+	}
+
+	private void walkToChop(){
+
+	}
+
+	private void bank(){
+
+	}
+
+	private void walkToBank(){
+
+	}
+
+	private void drop(){
+
+	}
+
+	@Override
+	public void onLoop() {
+		setLoopDelay(250, 401);
+		switch (getCurrentState()){
+			case CHOP:
+				chop();
+				break;
+			case WALK_TO_CHOP:
+				walkToChop();
+				break;
+			case BANK:
+				bank();
+				break;
+			case WALK_TO_BANK:
+				walkToBank();
+				break;
+			case DROP:
+				drop();
+				break;
+		}
+	}
+
 }
